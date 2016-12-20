96323d463cf8d7ecb354f6f507b292979c40e1a9
Salanoid <salanoid@gmail.com>
2016-12-21 00:15:17 +0200

    HomeController OOP

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,2 +1,9 @@
 <?php
-    require('./views/home.php');
+    class HomeController {
+        public function index() {
+            require('./views/home.php');
+        }
+    }
+    
+    $controller = new HomeController;
+    $controller->index();
